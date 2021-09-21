e92fa7d63ab9d3da9677de04be94bc63b41f69e0
emikhay <ekaterina.mikhaylova@positrigo.com>
2021-09-21 16:37:09 +0200

    in find_ML_normfactors3D.cxx changed num_transaxial/axial_crystals_per_block calculation to a super block calculation

--- src/utilities/find_ML_normfactors3D.cxx
+++ src/utilities/find_ML_normfactors3D.cxx
@@ -117,12 +117,22 @@ int main(int argc, char **argv)
   const int num_axial_blocks =
     measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
     get_num_axial_blocks();
+
+    // katya start
+    //const int num_transaxial_crystals_per_block =
+    //measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+    //get_num_transaxial_crystals_per_block();
+    //const int num_axial_crystals_per_block =
+    //measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+    //get_num_axial_crystals_per_block();
+
     const int num_transaxial_crystals_per_block =
     measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
-    get_num_transaxial_crystals_per_block();
+    get_num_transaxial_crystals_per_block() * num_transaxial_blocks;
     const int num_axial_crystals_per_block =
     measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
-    get_num_axial_crystals_per_block();
+    get_num_axial_crystals_per_block() * num_axial_blocks;
+    // katya stop
 
 
     
